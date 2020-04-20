22f9e6ee0c7fa9ddc4e7d4eb0f3132c532e5ba29
notebook <krawmaciej@gmail.com>
2020-04-21 01:04:33 +0200

    boiler plate

---
+++ src/test/java/cards/CardsGameApplicationTests.java
@@ -0,0 +1,13 @@
+package cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
